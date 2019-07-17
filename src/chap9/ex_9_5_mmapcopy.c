bc867f879fc5bc663ee7aa5279d3b954ef263b1c
Richard Chien <richardchienthebest@gmail.com>
2019-07-17 20:43:48 +0800

    Use fstat instead of stat

--- src/chap9/ex_9_5_mmapcopy.c
+++ src/chap9/ex_9_5_mmapcopy.c
@@ -10,7 +10,7 @@ void main_ex_9_5(int argc, char *argv[]) {
     const char *filename = "../README.md";
     int fd = open(filename, O_RDONLY);
     struct stat st;
-    Stat(filename, &st);
+    Fstat(fd, &st);
     char *ptr = (char *) Mmap(NULL, st.st_size, PROT_READ, MAP_PRIVATE | MAP_FILE, fd, 0);
     write(STDOUT_FILENO, ptr, st.st_size);
 }
